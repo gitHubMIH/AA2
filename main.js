c16e88744fc1800814804bd1718c5353c9cc663d
gitHubMIH <114809703+gitHubMIH@users.noreply.github.com>
2022-10-14 16:29:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+
+}
+function setup() {
+  canvas=createCanvas(1440,500);
+  rect(30, 20, 55, 55, 20, 300, 5, 333, 666, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 84151456151151515615151515151561515);
+}
+function draw() {
+
+}
+function takeSnapshot() {
+  save("captura.png");
+}
